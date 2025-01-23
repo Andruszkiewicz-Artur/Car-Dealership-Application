741bf170d9785ee3e4088818b1b74189bec89aa7
Artur Andruszkiewicz <andruszkiewiczarturmobiledev@gmail.com>
2025-01-24 00:15:28 +0100

    chore: - make network call

--- app/src/main/java/com/andruszkiewicz/cardealershipapplication/networking/service/CarDealershipService.kt
+++ app/src/main/java/com/andruszkiewicz/cardealershipapplication/networking/service/CarDealershipService.kt
@@ -3,10 +3,14 @@ package com.andruszkiewicz.cardealershipapplication.networking.service
 import com.andruszkiewicz.cardealershipapplication.networking.CarDto
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface CarDealershipService {
 
     @GET("cars")
     suspend fun getCars(): Response<List<CarDto>>
+
+    @GET("cars/{id}")
+    suspend fun getCar(@Path("id") id: Int): Response<CarDto>
 
 }
